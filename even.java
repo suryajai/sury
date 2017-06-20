1a196b56f0c257d7121ee972956344f85f85c8c8
suryajai <arasidpharm@gmail.com>
2017-06-20 15:38:37 +0530

    Update even.java

--- even.java
+++ even.java
@@ -5,13 +5,13 @@ import java.io.*;
 public class evenodd
  {
 	public static void main(String args[])
-	{int b;
+	{int no;
 	Scanner in=new Scanner(System.in);
-	System.out.println("Enter the value of b");
-	b=in.nextInt();
-	if(b%2==0){
+	System.out.println("Enter any number");
+	no=in.nextInt();
+	if(no%2==0){
 		System.out.println("The given no is even");
-	}if(b%2!=0){
+	}if(no%2!=0){
 		System.out.println("The given no is odd");
 	}
 	}
